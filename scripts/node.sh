18546c99d6fe73be52310c8155e0d2e6ba31e648
ozrlz <oscar.cucei@gmail.com>
2018-05-02 23:02:59 -0500

    [REF] Make the line shorter :p

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 # Add the node to the swarm cluster
-sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@manager 'docker swarm join-token worker' | grep -e "docker" | sudo /bin/bash
+sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@origin \
+'docker swarm join-token worker' | grep -e "docker" | sudo /bin/bash
 
