ce73ea443461d0a0bb1413c8b9dde9749559a201
ozrlz <oscar.cucei@gmail.com>
2018-05-02 23:03:38 -0500

    [IMP] Added extra node as manager

--- scripts/manager.sh
+++ scripts/manager.sh
@@ -1,2 +1,13 @@
-# Initialize the docker swarm cluster
-docker swarm init --advertise-addr 192.168.10.10
+# Initialize the docker swarm cluster only if the host is the origin
+if [ $(hostname) == "origin" ]
+then
+    docker swarm init --advertise-addr 192.168.10.10
+fi
+
+# If the node is a manager, add it as manager to the cluster
+hostname | grep -e "manager" > /dev/null 2>&1
+if [ $? == 0 ]
+then
+    sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@origin \
+    'docker swarm join-token manager' | grep -e "docker" | sudo /bin/bash
+fi
